faf988469eb6b243963794c2c604be8d1b1f524f
ludwig.eisgruber <ludwig.eisgruber@yathos.de>
2015-11-11 16:04:07 +0100

    UPDATE: requesting 1 byte from 2 slaves on adress 8 and 9 in a loop

--- playground/communicationmaster/communicationmaster.ino
+++ playground/communicationmaster/communicationmaster.ino
@@ -14,12 +14,27 @@ void loop() {
 
   Wire.requestFrom(8,1); //request 1 byte (currentAngle) from slave device #8
   //adress 0-7 are reserved
-  Serial.println("check");
+  Serial.println("check1");
   while(Wire.available()){//slave may send less then requested
   //digitalWrite(13, HIGH);    // turn the LED off by making the voltage LOW
     
     byte c = Wire.read();//receive a btye as character
-    Serial.println(c);//print the character
+    Serial.print(c);//print the character
+    
   }
+  Serial.println("\nend1");
+  delay(3000);
+
+  Wire.requestFrom(9,1); //request 1 byte (currentAngle) from slave device #8
+  //adress 0-7 are reserved
+  Serial.println("check2");
+  while(Wire.available()){//slave may send less then requested
+  //digitalWrite(13, HIGH);    // turn the LED off by making the voltage LOW
+    
+    byte c = Wire.read();//receive a btye as character
+    Serial.print(c);//print the character
+    
+  }
+  Serial.println("\nend2");
   delay(3000);
 }
