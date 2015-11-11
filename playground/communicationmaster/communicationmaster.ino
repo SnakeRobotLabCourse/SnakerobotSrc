0442bad206d576e93cf4c02586908ee33717af52
ludwig.eisgruber <ludwig.eisgruber@yathos.de>
2015-11-11 16:28:57 +0100

    UPDATE: sending request from master to slave, dependent on input adress (8 or 9 from serial input)

--- playground/communicationmaster/communicationmaster.ino
+++ playground/communicationmaster/communicationmaster.ino
@@ -1,5 +1,9 @@
 #include<Wire.h>
 
+String inputString = "";
+boolean inputComplete;
+
+
 void setup() {
  
    Wire.begin();
@@ -12,29 +16,49 @@ void setup() {
 
 void loop() {
 
-  Wire.requestFrom(8,1); //request 1 byte (currentAngle) from slave device #8
-  //adress 0-7 are reserved
-  Serial.println("check1");
-  while(Wire.available()){//slave may send less then requested
-  //digitalWrite(13, HIGH);    // turn the LED off by making the voltage LOW
-    
-    byte c = Wire.read();//receive a btye as character
-    Serial.print(c);//print the character
-    
-  }
-  Serial.println("\nend1");
-  delay(3000);
+  serialEvent();
 
-  Wire.requestFrom(9,1); //request 1 byte (currentAngle) from slave device #8
-  //adress 0-7 are reserved
-  Serial.println("check2");
-  while(Wire.available()){//slave may send less then requested
-  //digitalWrite(13, HIGH);    // turn the LED off by making the voltage LOW
+  if(inputComplete){
+    byte adressByte = inputString[0];
+    Serial.println("givvenbyte");
+    Serial.println(adressByte);
     
-    byte c = Wire.read();//receive a btye as character
-    Serial.print(c);//print the character
-    
+    inputComplete = false;
+    inputString = "";
+    if(adressByte == 56){ //ascii 56 = 8, 57=9
+        Serial.println("adress 8");
+       Wire.requestFrom(8,1); 
+       read();
+    } else if (adressByte == 57){
+        Serial.println("adress 9");
+       Wire.requestFrom(9,1); 
+       read();
+    } else{
+        Serial.println("no know adress");
+    }
   }
-  Serial.println("\nend2");
+
   delay(3000);
 }
+
+void read(){
+  while(Wire.available()){//slave may send less then requested
+  //digitalWrite(13, HIGH);    // turn the LED off by making the voltage LOW
+    
+    byte c = Wire.read();//receive a btye as character
+    Serial.print(c);//print the character
+    
+  }
+}
+
+void serialEvent(){
+  while(Serial.available()){
+      char inChar = (char)Serial.read();
+      if(inChar=='\n'){
+        inputComplete = true;
+      } else{
+        inputString+=inChar;
+      }
+ }
+}
+
