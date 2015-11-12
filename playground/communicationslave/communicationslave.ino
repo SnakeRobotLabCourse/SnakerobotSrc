26c34a18748615b16376039748fa6d9df8496d65
Ben Schneider <schneibe@in.tum.de>
2015-11-12 20:09:00 +0100

    refactored the slave code and added comments to it

--- playground/communicationslave/communicationslave.ino
+++ playground/communicationslave/communicationslave.ino
@@ -1,57 +1,53 @@
 #include<Wire.h>
 
+//Status: Slave can receive (1 byte at the moment) and send data from/to master
+//Next Steps: parse the received data and control the servo which is connected to the slave
+
 String inputString = "";
 boolean inputComplete;
 
 byte buf[3] = {12,10,18};
-byte adrress = 9;
+byte address = 9;
 
 void setup() {
   Serial.begin(9600);
-  inputString.reserve(200);//reserve 200 bytes fro the inputstring
-  Wire.begin(adrress);
-  Wire.onRequest(requestEvent);
-  Wire.onReceive(receiveEvent);
+  inputString.reserve(200);       //reserve 200 bytes fro the inputstring
+  Wire.begin(address);
+  Wire.onRequest(requestEvent);   //register the handler for a request event  (Master: Wire.requestFrom(...))
+  Wire.onReceive(receiveEvent);   //register the handler for a receive event  (Master: Wire.write(...))
 }
 
 void loop() {
-  delay(100);
+  delay(100);   //we dont need to do anything here, as the handlers are called on incoming events
 }
 
+//callback which is called, when the slave receives a request from the master 
+//(master requests the current data from the slave, e.g. current servo angle or speed)
 void requestEvent(){
   Serial.println("Pls input string");
   serialEvent();
-  Wire.write(inputString.c_str());//respond with message of 6 bytes as expected by master
+  Wire.write(inputString.c_str());
   inputComplete = false;
   inputString = "";
 } 
 
+//method for reading serial console input
 void serialEvent(){
   while(Serial.available()){
       char inChar = (char)Serial.read();
-      inputString+=inChar;
-
+      inputString+=inChar;      //adds console input to the inputString
       if(inChar=='\n'){
-        inputComplete = true;
+        inputComplete = true;   // input string is ready if user enters "enter" 
       }
   }
 }
+//callback which is called, when the slave receives a receive event from the master
+//(master sends a command to slave, e.g. new servo angle or speed)
   void receiveEvent(int howMany) {
-  while (1 < Wire.available()) { // loop through all but the last
-    char c = Wire.read(); // receive byte as a character
-    //String resultCharString = "Slave 9 received char " + c;
-    //Serial
-    
-    Serial.print(c);         // print the character
+  while (1 < Wire.available()) {  // loop through all but the last
+    char c = Wire.read();         // receive byte as a character   
+    Serial.print(c);
   }
-  int x = Wire.read();    // receive byte as an integer
-  //String resultIntString = "Slave 9 received int " + x;
+  int x = Wire.read();            // receive byte as an integer
   Serial.println(x);
-  
-  //serialEvent();
-  //Serial.println(inputString.c_str());
-  //Wire.write("blub");//respond with message of 6 bytes as expected by master
-  //inputComplete = false;
-  //inputString = "";
-}
- 
+ }
