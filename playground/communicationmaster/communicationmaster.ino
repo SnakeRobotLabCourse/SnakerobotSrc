c03094d78e3970dd51fd5b8972de5f52f6fa8178
ludwig.eisgruber <ludwig.eisgruber@yathos.de>
2015-11-13 12:43:56 +0100

    UPDATE: turning servo right form master

--- playground/communicationmaster/communicationmaster.ino
+++ playground/communicationmaster/communicationmaster.ino
@@ -2,7 +2,7 @@
 
 String inputString = "";
 boolean inputComplete;
-
+byte slaves[5] = {8,9,10,11,12};
 
 void setup() {
  
@@ -31,14 +31,17 @@ void loop() {
          Wire.endTransmission();     // stop transmitting
          delay(10);
        Wire.requestFrom(8,1); 
-    } else if (adressByte == 57){
-        Serial.println("adress 9");
-        Wire.beginTransmission(9); // transmit to device #44 (0x2c)
-                 // device address is specified in datasheet
-        Wire.write(inputString.c_str());             // sends value byte  
-        Wire.endTransmission();     // stop transmitting
-        delay(10);//WITHOUT THIS IT FAILS
-        Wire.requestFrom(9, 10);
+    } else if (adressByte == 48){
+      trasnmitServoCommand(adressByte, 1, 127);
+        //Serial.println("adress 9");
+        //Wire.beginTransmission(9);
+        //for(int i=1; i<inputString.length(); i++){
+          // dont send address
+         //Wire.write(inputString[i]);
+        //}
+        //Wire.endTransmission();     // stop transmitting
+        //delay(10);//WITHOUT THIS IT FAILS
+        //Wire.requestFrom(9, 10);
      } else{
         Serial.println("no know adress");
     }
@@ -68,7 +71,26 @@ void read(){
     
   }
   
-  Serial.println("after whileloop");
+}
+
+void trasnmitServoCommand(byte adress, byte direction, byte speed){
+  byte slaveindex = adress-48;
+  if(slaveindex>=0){
+      Serial.println("slave index");
+     Serial.println(slaveindex);
+     Wire.beginTransmission(slaves[slaveindex]);
+     Wire.write('d'); //send 'd' as byte
+     Wire.write(direction); //direction 0 = stop, 1 = clockwise, 2 = counterclockwise
+     Wire.write('s'); //send 'd' as byte
+     Wire.write(speed);
+     Wire.endTransmission();     // stop transmitting
+     delay(10);
+  } else{
+    Serial.println("wrong slave index");
+    Serial.println(slaveindex);
+  }
+  
+ 
 }
 
 void serialEvent(){
